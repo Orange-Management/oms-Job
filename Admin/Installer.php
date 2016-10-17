96bef074a679c3d5c2fb06e62ff26f577aac3531
Dennis Eichhorn <spl1nes.com@googlemail.com>
2016-10-17 21:08:33 +0200

    Create table draft

--- Admin/Installer.php
+++ Admin/Installer.php
@@ -43,6 +43,41 @@ class Installer extends InstallerAbstract
 
         switch ($dbPool->get('core')->getType()) {
             case DatabaseType::MYSQL:
+                $dbPool->get('core')->con->prepare(
+                    'CREATE TABLE if NOT EXISTS `' . $dbPool->get('core')->prefix . 'job` (
+                            `job_id` int(11) NOT NULL AUTO_INCREMENT,
+                            `job_name` varchar(50) NOT NULL,
+                            `job_status` int(11) NOT NULL,
+                            `job_desc` varchar(100) DEFAULT NULL,
+                            `job_created` datetime DEFAULT NULL,
+                            `job_created_by` int(11) DEFAULT NULL,
+                            PRIMARY KEY (`job_id`),
+                            KEY `job_created_by` (`job_created_by`)
+                        )ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;'
+                )->execute();
+
+                $dbPool->get('core')->con->prepare(
+                    'ALTER TABLE `' . $dbPool->get('core')->prefix . 'job`
+                            ADD CONSTRAINT `' . $dbPool->get('core')->prefix . 'job_ibfk_1` FOREIGN KEY (`job_created_by`) REFERENCES `' . $dbPool->get('core')->prefix . 'account` (`account_id`);'
+                )->execute();
+
+                $dbPool->get('core')->con->prepare(
+                    'CREATE TABLE if NOT EXISTS `' . $dbPool->get('core')->prefix . 'job_log` (
+                            `job_log_id` int(11) NOT NULL AUTO_INCREMENT,
+                            `job_log_status` int(3) NOT NULL,
+                            `job_log_message` text NOT NULL,
+                            `job_log_job` int(11) NOT NULL,
+                            `job_log_created` datetime DEFAULT NULL,
+                            PRIMARY KEY (`job_log_id`),
+                            KEY `job_log_job` (`job_log_job`)
+                        )ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;'
+                )->execute();
+
+                $dbPool->get('core')->con->prepare(
+                    'ALTER TABLE `' . $dbPool->get('core')->prefix . 'job_log`
+                            ADD CONSTRAINT `' . $dbPool->get('core')->prefix . 'job_log_ibfk_1` FOREIGN KEY (`job_log_job`) REFERENCES `' . $dbPool->get('core')->prefix . 'job` (`job_id`);'
+                )->execute();
+
                 break;
         }
     }
