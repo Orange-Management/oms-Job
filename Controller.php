ccad08d3d0db98820ac05774877716d85c5a182a
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-02-12 21:43:10 +0100

    Bulkl path fix reverse

---
+++ Controller.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.1
+ *
+ * @category   TBD
+ * @package    TBD
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @copyright  Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ */
+namespace Modules\Job;
+
+use phpOMS\Message\RequestAbstract;
+use phpOMS\Message\ResponseAbstract;
+use phpOMS\Module\ModuleAbstract;
+use phpOMS\Module\WebInterface;
+use phpOMS\Views\View;
+use phpOMS\Utils\TaskSchedule\SchedulerFactory;
+use phpOMS\Utils\TaskSchedule\SchedulerAbstract;
+
+/**
+ * Task class.
+ *
+ * @category   Modules
+ * @package    Modules\Job
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @license    OMS License 1.0
+ * @link       http://orange-management.com
+ * @since      1.0.0
+ */
+class Controller extends ModuleAbstract implements WebInterface
+{
+
+    /**
+     * Module path.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    /* public */ const MODULE_PATH = __DIR__;
+
+    /**
+     * Module version.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    /* public */ const MODULE_VERSION = '1.0.0';
+
+    /**
+     * Module name.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    /* public */ const MODULE_NAME = 'Job';
+
+    /**
+     * Providing.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    protected static $providing = [];
+
+    /**
+     * Dependencies.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    protected static $dependencies = [
+    ];
+
+    /**
+     * @param RequestAbstract  $request  Request
+     * @param ResponseAbstract $response Response
+     * @param mixed            $data     Generic data
+     *
+     * @return \Serializable
+     *
+     * @since  1.0.0
+     * @author Dennis Eichhorn <d.eichhorn@oms.com>
+     */
+    public function viewJobList(RequestAbstract $request, ResponseAbstract $response, $data = null) : \Serializable
+    {
+        $view = new View($this->app, $request, $response);
+        $view->setTemplate('/Modules/Job/Theme/Backend/job-dashboard');
+        $view->addData('nav', $this->app->moduleManager->get('Navigation')->createNavigationMid(1005701001, $request, $response));
+
+        SchedulerAbstract::setBin($this->app->getConfig()['jobs']['path']);
+        $scheduler = SchedulerFactory::create();
+        $jobs = $scheduler->getAllByName('Adobe', false);
+
+        $view->addData('jobs', $jobs);
+
+        return $view;
+    }
+
+    /**
+     * @param RequestAbstract  $request  Request
+     * @param ResponseAbstract $response Response
+     * @param mixed            $data     Generic data
+     *
+     * @return \Serializable
+     *
+     * @since  1.0.0
+     * @author Dennis Eichhorn <d.eichhorn@oms.com>
+     */
+    public function viewJobCreate(RequestAbstract $request, ResponseAbstract $response, $data = null) : \Serializable
+    {
+        $view = new View($this->app, $request, $response);
+        $view->setTemplate('/Modules/Job/Theme/Backend/job-create');
+        $view->addData('nav', $this->app->moduleManager->get('Navigation')->createNavigationMid(1005701001, $request, $response));
+
+        return $view;
+    }
+
+    /**
+     * @param RequestAbstract  $request  Request
+     * @param ResponseAbstract $response Response
+     * @param mixed            $data     Generic data
+     *
+     * @return \Serializable
+     *
+     * @since  1.0.0
+     * @author Dennis Eichhorn <d.eichhorn@oms.com>
+     */
+    public function viewJob(RequestAbstract $request, ResponseAbstract $response, $data = null) : \Serializable
+    {
+        $view = new View($this->app, $request, $response);
+        $view->setTemplate('/Modules/Job/Theme/Backend/job-single');
+        $view->addData('nav', $this->app->moduleManager->get('Navigation')->createNavigationMid(1005701001, $request, $response));
+
+        SchedulerAbstract::setBin($this->app->getConfig()['jobs']['path']);
+        $scheduler = SchedulerFactory::create();
+        $job = $scheduler->getAllByName('Adobe', false);
+
+        $view->addData('job', end($job));
+
+        return $view;
+    }
+
+}
---
+++ Theme/Backend/job-single.tpl.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.1
+ *
+ * @category   TBD
+ * @package    TBD
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @copyright  Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ */
+/**
+ * @var \phpOMS\Views\View         $this
+ */
+$job      = $this->getData('job');
+
+echo $this->getData('nav')->render(); ?>
+
+<div class="row">
+    <div class="col-xs-12">
+        <section class="box wf-100">
+            <header><h1><?= $job->getId(); ?></h1></header>
+            <div class="inner">
+            <table class="list w-100">
+                    <tr>
+                        <td><?= $this->getText('Status'); ?>
+                        <td><i class="fa fa-anchor"></i>
+                        <td class="wf-100"><?= $job->getStatus(); ?>
+                    <tr>
+                        <td><?= $this->getText('Run'); ?>
+                        <td><i class="fa fa-anchor"></i>
+                        <td><?= $job->getRun(); ?>
+                    <tr>
+                        <td><?= $this->getText('LastRunTime'); ?>
+                        <td><i class="fa fa-anchor"></i>
+                        <td><?= $job->getLastRunTime() !== null ? $job->getLastRunTime()->format('Y-m-d') : ''; ?>
+                    <tr>
+                        <td><?= $this->getText('NextRunTime'); ?>
+                        <td><i class="fa fa-anchor"></i>
+                        <td><?= $job->getNextRunTime() !== null ? $job->getNextRunTime()->format('Y-m-d') : ''; ?>
+                    <tr>
+                        <td><?= $this->getText('Description'); ?>
+                        <td><i class="fa fa-anchor"></i>
+                        <td><blockquote><?= $job->getComment(); ?></blockquote>
+            </table>
+                
+            </div>
+        </section>
+    </div>
+</div>
