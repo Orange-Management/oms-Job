42a683291a164d3c672406935cb0d886df39765c
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-04-16 20:38:39 +0200

    Make all tables red

--- Theme/Backend/job-dashboard.tpl.php
+++ Theme/Backend/job-dashboard.tpl.php
@@ -23,7 +23,7 @@ echo $this->getData('nav')->render(); ?>
 <div class="row">
     <div class="col-xs-12">
         <div class="box wf-100">
-            <table class="table">
+            <table class="table red">
                 <caption><?= $this->getText('Job'); ?></caption>
                 <thead>
                 <td><?= $this->getText('Status'); ?>
