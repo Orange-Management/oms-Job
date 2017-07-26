1e8bcf69d68ebb15ce99fac72a55222e8533d62e
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-07-26 17:48:08 +0200

    Fix file author and copyright

--- Theme/Backend/job-create.tpl.php
+++ Theme/Backend/job-create.tpl.php
@@ -6,8 +6,7 @@
  *
  * @category   TBD
  * @package    TBD
- * @author     OMS Development Team <dev@oms.com>
- * @copyright  2013 Dennis Eichhorn
+ * @copyright  Dennis Eichhorn
  * @license    OMS License 1.0
  * @version    1.0.0
  * @link       http://orange-management.com
