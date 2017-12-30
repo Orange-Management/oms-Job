baa8fcfc8dd1b9e86c32153f52c9cafa506f9c2d
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-12-30 12:45:02 +0100

    Remove @category and replace with @package

--- Controller.php
+++ Controller.php
@@ -4,7 +4,7 @@
  *
  * PHP Version 7.1
  *
- * @category   TBD
+ * @package    TBD
  * @copyright  Dennis Eichhorn
  * @license    OMS License 1.0
  * @version    1.0.0
@@ -24,8 +24,7 @@ use phpOMS\Utils\TaskSchedule\SchedulerAbstract;
 /**
  * Task class.
  *
- * @category   Modules
- * @package    Modules\Job
+ * @package    Modules
  * @license    OMS License 1.0
  * @link       http://website.orange-management.de
  * @since      1.0.0
