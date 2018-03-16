511a7794bd6d9cf1079373a8921ef88aa83f247c
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-03-16 12:39:55 +0100

    docblock typehint fix

--- Controller.php
+++ Controller.php
@@ -68,7 +68,7 @@ class Controller extends ModuleAbstract implements WebInterface
     /**
      * Providing.
      *
-     * @var string
+     * @var string[]
      * @since 1.0.0
      */
     protected static $providing = [];
@@ -76,7 +76,7 @@ class Controller extends ModuleAbstract implements WebInterface
     /**
      * Dependencies.
      *
-     * @var string
+     * @var string[]
      * @since 1.0.0
      */
     protected static $dependencies = [
