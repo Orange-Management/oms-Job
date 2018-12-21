e11db115007c240b52592ff1625eefe272263e5f
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-12-21 20:22:10 +0100

    Remove default uninstall

--- Admin/Uninstaller.php
+++ Admin/Uninstaller.php
@@ -20,7 +20,7 @@ use phpOMS\Module\InfoManager;
 use phpOMS\Module\UninstallerAbstract;
 
 /**
- * Navigation class.
+ * Uninstaller class.
  *
  * @package    Modules\Job\Admin
  * @license    OMS License 1.0
@@ -29,22 +29,4 @@ use phpOMS\Module\UninstallerAbstract;
  */
 class Uninstaller extends UninstallerAbstract
 {
-
-    /**
-     * {@inheritdoc}
-     */
-    public static function uninstall(DatabasePool $dbPool, InfoManager $info) : void
-    {
-        parent::uninstall($dbPool, $info);
-
-        $query = new Builder($dbPool->get());
-
-        $query->prefix($dbPool->get()->getPrefix())->drop(
-            'task_element',
-            'task_account',
-            'task'
-        );
-
-        $dbPool->get()->con->prepare($query->toSql())->execute();
-    }
 }
