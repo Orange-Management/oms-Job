19cb949b38a627f56602bd3f65f954f59a16475f
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-03-14 14:15:13 +0100

    add tests

---
+++ tests/Admin/AdminTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.4
+ *
+ * @package    tests
+ * @copyright  2013 Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       https://orange-management.org
+ */
+declare(strict_types=1);
+
+namespace Modules\tests\Job\Admin;
+
+/**
+ * @internal
+ */
+class AdminTest extends \PHPUnit\Framework\TestCase
+{
+    protected const MODULE_NAME = 'Job';
+    protected const URI_LOAD = '';
+
+    use \Modules\tests\ModuleTestTrait;
+}
